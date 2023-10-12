95cf788cbf93b593dfe3ac5b938526718b5014d2
sovannarapov <sovannarapov9@gmail.com>
2023-10-12 14:06:06 +0700

    added bookmark crud api and added e2e test with pactum

--- src/bookmark/bookmark.module.ts
+++ src/bookmark/bookmark.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
-import { BookmarkController } from './bookmark.controller';
-import { BookmarkService } from './bookmark.service';
+import { BookmarkController } from './controllers/bookmark.controller';
+import { BookmarkService } from './services/bookmark.service';
 
 @Module({
   controllers: [BookmarkController],
